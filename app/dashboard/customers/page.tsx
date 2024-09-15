8632ed5bf31e3788291ee0db749b855386b7d41d
System Administrator <root@peters-mbp.mynetworksettings.com>
2024-09-15 02:06:42 -0400

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error unde
+      quidem sit. Similique libero tempora possimus velit itaque eius amet
+      suscipit, eos nulla obcaecati eligendi tenetur unde assumenda nobis sint!
+    </main>
+  );
+}
